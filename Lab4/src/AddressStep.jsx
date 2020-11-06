4d2e3fa0606834d0f652162676dcded419929219
bartoszluka <72729880+bartoszluka@users.noreply.github.com>
2020-11-06 17:55:14 +0100

    Address step refactored

--- Lab4/src/AddressStep.jsx
+++ Lab4/src/AddressStep.jsx
@@ -2,52 +2,40 @@ import React, { useState } from 'react';
 
 export default function AddressStep(props) {
 
-    const [deliveryStreetName, setDeliveryStreetName] = useState('');
-    const [deliveryZipCode, setDeliveryZipCode] = useState('');
-    const [deliveryCityName, setDeliveryCityName] = useState('');
-
     const [wrongDeliveryStreetName, setWrongDeliveryStreetName] = useState(false);
     const [wrongDeliveryZipCode, setWrongDeliveryZipCode] = useState(false);
     const [wrongDeliveryCityName, setWrongDeliveryCityName] = useState(false);
-
-    const [isInvoiceTheSameAsDelivery, setIsInvoiceTheSameAsDelivery] = useState(false);
-
-    const [invoiceStreetName, setInvoiceStreetName] = useState('');
-    const [invoiceZipCode, setInvoiceZipCode] = useState('');
-    const [invoiceCityName, setInvoiceCityName] = useState('');
 
     const [wrongInvoiceStreetName, setWrongInvoiceStreetName] = useState(false);
     const [wrongInvoiceZipCode, setWrongInvoiceZipCode] = useState(false);
     const [wrongInvoiceCityName, setWrongInvoiceCityName] = useState(false);
 
-
-
     function clickNext() {
         let isCorrect = true;
 
         //check delivery info
-        if (deliveryStreetName === '') {
+        if (props.deliveryStreetName === '') {
             setWrongDeliveryStreetName(true);
             isCorrect = false;
         }
         else
             setWrongDeliveryStreetName(false);
 
-        if (deliveryZipCode === '' || !(/^[0-9]{2}-[0-9]{3}/.test(deliveryZipCode))) {
+        if (props.deliveryZipCode === '' || !(/^[0-9]{2}-[0-9]{3}/.test(props.deliveryZipCode))) {
             setWrongDeliveryZipCode(true);
             isCorrect = false;
         }
         else
             setWrongDeliveryZipCode(false);
 
-        if (deliveryCityName === '') {
+        if (props.deliveryCityName === '') {
             setWrongDeliveryCityName(true);
             isCorrect = false;
         }
         else
             setWrongDeliveryCityName(false);
 
-        if (isInvoiceTheSameAsDelivery) {
+        if (props.isInvoiceTheSameAsDelivery) {
             if (!isCorrect)
                 alert("fill all inputs");
             else
@@ -56,28 +44,26 @@ export default function AddressStep(props) {
         //now check invoice info
         else {
 
-            if (invoiceStreetName === '') {
+            if (props.invoiceStreetName === '') {
                 setWrongInvoiceStreetName(true);
                 isCorrect = false;
             }
             else
                 setWrongInvoiceStreetName(false);
 
-            if (invoiceZipCode === '' || !(/^[0-9]{2}-[0-9]{3}/.test(invoiceZipCode))) {
+            if (props.invoiceZipCode === '' || !(/^[0-9]{2}-[0-9]{3}/.test(props.invoiceZipCode))) {
                 setWrongInvoiceZipCode(true);
                 isCorrect = false;
             }
             else
                 setWrongInvoiceZipCode(false);
 
-            if (invoiceCityName === '') {
+            if (props.invoiceCityName === '') {
                 setWrongInvoiceCityName(true);
                 isCorrect = false;
             }
             else
                 setWrongInvoiceCityName(false);
-
-
 
             if (!isCorrect)
                 alert("fill all inputs");
@@ -88,25 +74,25 @@ export default function AddressStep(props) {
     }
 
     function setInvoiceInfo(event) {
-        setIsInvoiceTheSameAsDelivery(event.target.checked);
-        setInvoiceStreetName(deliveryStreetName);
-        setInvoiceZipCode(deliveryZipCode);
-        setInvoiceCityName(deliveryCityName);
+        props.setIsInvoiceTheSameAsDelivery(event.target.checked);
+        props.setInvoiceStreetName(props.deliveryStreetName);
+        props.setInvoiceZipCode(props.deliveryZipCode);
+        props.setInvoiceCityName(props.deliveryCityName);
     }
 
     return (
         <div>
             Delivery address
             <form>
-                <input type="text" value={deliveryStreetName} onChange={(event) => setDeliveryStreetName(event.target.value)}
+                <input type="text" value={props.deliveryStreetName} onChange={(event) => props.setDeliveryStreetName(event.target.value)}
                     placeholder="street" />
                 {wrongDeliveryStreetName ? <span> Street name must not be empty</span> : null}
                 <br />
-                <input type="text" value={deliveryZipCode} onChange={(event) => setDeliveryZipCode(event.target.value)}
+                <input type="text" value={props.deliveryZipCode} onChange={(event) => props.setDeliveryZipCode(event.target.value)}
                     placeholder="zip code (12-345)" />
                 {wrongDeliveryZipCode ? <span> Wrong zip code! Accepted format is 12-345</span> : null}
                 <br />
-                <input type="text" value={deliveryCityName} onChange={(event) => setDeliveryCityName(event.target.value)}
+                <input type="text" value={props.deliveryCityName} onChange={(event) => props.setDeliveryCityName(event.target.value)}
                     placeholder="city" />
                 {wrongDeliveryCityName ? <span> City name must not be empty</span> : null}
             </form>
@@ -118,16 +104,16 @@ export default function AddressStep(props) {
                 keep the same as delivery
             </label>
             <form>
-                <input type="text" value={isInvoiceTheSameAsDelivery ? deliveryStreetName : invoiceStreetName} onChange={(event) => setInvoiceStreetName(event.target.value)}
-                    placeholder="street" disabled={isInvoiceTheSameAsDelivery} />
+                <input type="text" value={props.isInvoiceTheSameAsDelivery ? props.deliveryStreetName : props.invoiceStreetName} onChange={(event) => props.setInvoiceStreetName(event.target.value)}
+                    placeholder="street" disabled={props.isInvoiceTheSameAsDelivery} />
                 {wrongInvoiceStreetName ? <span> Street name must not be empty</span> : null}
                 <br />
-                <input type="text" value={isInvoiceTheSameAsDelivery ? deliveryZipCode : invoiceZipCode} onChange={(event) => setInvoiceZipCode(event.target.value)}
-                    placeholder="zip code (12-345)" disabled={isInvoiceTheSameAsDelivery} />
+                <input type="text" value={props.isInvoiceTheSameAsDelivery ? props.deliveryZipCode : props.invoiceZipCode} onChange={(event) => props.setInvoiceZipCode(event.target.value)}
+                    placeholder="zip code (12-345)" disabled={props.isInvoiceTheSameAsDelivery} />
                 {wrongInvoiceZipCode ? <span> Wrong zip code! Accepted format is 12-345</span> : null}
                 <br />
-                <input type="text" value={isInvoiceTheSameAsDelivery ? deliveryCityName : invoiceCityName} onChange={(event) => setInvoiceCityName(event.target.value)}
-                    placeholder="city" disabled={isInvoiceTheSameAsDelivery} />
+                <input type="text" value={props.isInvoiceTheSameAsDelivery ? props.deliveryCityName : props.invoiceCityName} onChange={(event) => props.setInvoiceCityName(event.target.value)}
+                    placeholder="city" disabled={props.isInvoiceTheSameAsDelivery} />
                 {wrongInvoiceCityName ? <span> City name must not be empty</span> : null}
             </form>
             <button onClick={clickNext}>Next</button>
--- Lab4/src/CustomerForm.jsx
+++ Lab4/src/CustomerForm.jsx
@@ -6,10 +6,21 @@ import SummaryStep from './SummaryStep.jsx'
 export default function CustomerForm() {
     const [activeStep, setActiveStep] = useState(0);
 
+    //name step props
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [emailAddress, setEmailAddress] = useState('');
 
+    //address step props
+    const [deliveryStreetName, setDeliveryStreetName] = useState('');
+    const [deliveryZipCode, setDeliveryZipCode] = useState('');
+    const [deliveryCityName, setDeliveryCityName] = useState('');
+
+    const [isInvoiceTheSameAsDelivery, setIsInvoiceTheSameAsDelivery] = useState(false);
+
+    const [invoiceStreetName, setInvoiceStreetName] = useState('');
+    const [invoiceZipCode, setInvoiceZipCode] = useState('');
+    const [invoiceCityName, setInvoiceCityName] = useState('');
 
     const nextStep = () => {
         setActiveStep(activeStep + 1);
@@ -18,11 +29,18 @@ export default function CustomerForm() {
         <div>
             {activeStep === 0
                 ? <NameStep nextStep={nextStep}
-                firstName={firstName} setFirstName={setFirstName} 
-                lastName={lastName} setLastName={setLastName}
-                emailAddress={emailAddress} setEmailAddress={setEmailAddress}/>
+                    firstName={firstName} setFirstName={setFirstName}
+                    lastName={lastName} setLastName={setLastName}
+                    emailAddress={emailAddress} setEmailAddress={setEmailAddress} />
                 : activeStep === 1
-                    ? <AddressStep nextStep={nextStep} 
+                    ? <AddressStep nextStep={nextStep}
+                        deliveryStreetName={deliveryStreetName} setDeliveryStreetName={setDeliveryStreetName}
+                        deliveryZipCode={deliveryZipCode} setDeliveryZipCode={setDeliveryZipCode}
+                        deliveryCityName={deliveryCityName} setDeliveryCityName={setDeliveryCityName}
+                        isInvoiceTheSameAsDelivery={isInvoiceTheSameAsDelivery} setIsInvoiceTheSameAsDelivery={setIsInvoiceTheSameAsDelivery}
+                        invoiceStreetName={invoiceStreetName} setInvoiceStreetName={setInvoiceStreetName}
+                        invoiceZipCode={invoiceZipCode} setInvoiceZipCode={setInvoiceZipCode}
+                        invoiceCityName={invoiceCityName} setInvoiceCityName={setInvoiceCityName}
                     />
                     : <SummaryStep />
             }
