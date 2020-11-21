4ff85c76d274ff55af5c6ff88450a6d237d4a105
bartoszluka <72729880+bartoszluka@users.noreply.github.com>
2020-11-21 18:50:45 +0100

    state works, paintBoard is ugly

--- lab6/src/Board.jsx
+++ lab6/src/Board.jsx
@@ -6,23 +6,23 @@ import Tile from './Tile';
 const mapStateToProps = (state) => {
     return { snake: state.snake }
 }
-const mapDispatchToProps = (dispatch) =>{
+const mapDispatchToProps = (dispatch) =>({
     moveLeft: ()=> dispatch({type:"MOVE_LEFT"})
-}
+})
 
-export function Board() {
+export function Board(props) {
     
     function paintBoard(){
-        let grid = new Array(this.props.width);
-        for (let i = 0; i < this.props.width; i++){
-            grid[i]= new Array(this.props.height);
+        let grid = new Array(props.width);
+        for (let i = 0; i < props.width; i++){
+            grid[i]= new Array(props.height);
         }
-        for (let s in this.props.snake){
-            grid[this.props.snake[s].x][this.props.snake[s].y] = "green"
+        for (let s in props.snake){
+            grid[props.snake[s].x][props.snake[s].y] = "green"
         }
         return grid;
     }
-    return this.paintBoard().map(col => col.map(color=> {<Tile color={color} />}))
+    return paintBoard().map(col => col.map(color=> {<Tile color={color} />}))
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Board)
