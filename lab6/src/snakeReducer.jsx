f305f647fb94f79ad46d9a1690da985eda8e041d
bartoszluka <72729880+bartoszluka@users.noreply.github.com>
2020-11-23 00:27:39 +0100

    snake can't go past (hardcoded) walls

--- lab6/src/snakeReducer.jsx
+++ lab6/src/snakeReducer.jsx
@@ -1,19 +1,31 @@
 
 export default function snakeReducer(state = {
-    snake: [{ x: 1, y: 7 },{ x: 1, y: 8 }, { x: 1, y: 9 }]
+    snake: [{ x: 1, y: 7 }, { x: 1, y: 8 }, { x: 1, y: 9 }]
 }, action) {
     const head = state.snake[0];
     switch (action.type) {
         case "MOVE_LEFT":
+            if (head.y === 0) {
+                return state;
+            }
             state.snake.pop();
             return { ...state, snake: [{ x: head.x, y: head.y - 1 }, ...state.snake] };
         case "MOVE_RIGHT":
+            if (head.y === 9) {
+                return state;
+            }
             state.snake.pop();
             return { ...state, snake: [{ x: head.x, y: head.y + 1 }, ...state.snake] };
         case "MOVE_UP":
+            if (head.x === 0) {
+                return state;
+            }
             state.snake.pop();
             return { ...state, snake: [{ x: head.x - 1, y: head.y }, ...state.snake] };
         case "MOVE_DOWN":
+            if (head.x === 9) {
+                return state;
+            }
             state.snake.pop();
             return { ...state, snake: [{ x: head.x + 1, y: head.y }, ...state.snake] };
         default:
