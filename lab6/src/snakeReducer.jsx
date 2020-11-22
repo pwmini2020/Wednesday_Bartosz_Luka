ce7c43ac4d3b740e57d6b19776cc0520b4d41ffa
bartoszluka <72729880+bartoszluka@users.noreply.github.com>
2020-11-23 00:24:12 +0100

    snake can move

--- lab6/src/snakeReducer.jsx
+++ lab6/src/snakeReducer.jsx
@@ -1,21 +1,21 @@
 
 export default function snakeReducer(state = {
-    snake: [{ x: 1, y: 1 }, { x: 2, y: 1 }]
+    snake: [{ x: 1, y: 7 },{ x: 1, y: 8 }, { x: 1, y: 9 }]
 }, action) {
+    const head = state.snake[0];
     switch (action.type) {
         case "MOVE_LEFT":
-            // return { ...state, snake: [...state.snake, state.snake.push({ x: 0, y: 1 })] };
-            console.log('lewo');
-            return state;
+            state.snake.pop();
+            return { ...state, snake: [{ x: head.x, y: head.y - 1 }, ...state.snake] };
         case "MOVE_RIGHT":
-            console.log("prawo")
-            return state;
+            state.snake.pop();
+            return { ...state, snake: [{ x: head.x, y: head.y + 1 }, ...state.snake] };
         case "MOVE_UP":
-            console.log("gora")
-            return state;
+            state.snake.pop();
+            return { ...state, snake: [{ x: head.x - 1, y: head.y }, ...state.snake] };
         case "MOVE_DOWN":
-            console.log("dol")
-            return state;
+            state.snake.pop();
+            return { ...state, snake: [{ x: head.x + 1, y: head.y }, ...state.snake] };
         default:
             return state;
     }
