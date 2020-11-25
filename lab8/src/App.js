17ea0a0859e47f21190a13d7cbcc93d70a0078cf
bartoszluka <bartoszluka1@gmail.com>
2020-11-25 12:17:04 +0100

    adding router-dom

--- lab8/src/App.js
+++ lab8/src/App.js
@@ -1,4 +1,10 @@
 import React, { useEffect } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import EmployeeList from './EmployeeList'
 function App() {
   return (
